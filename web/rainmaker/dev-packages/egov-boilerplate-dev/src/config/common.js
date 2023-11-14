eb1353bdc866057fd6f0c7b09019f2dbebaef88c
erramesh000 <erramesh000@gmail.com>
2023-11-14 14:52:01 +0530

    Fixed issues google map

--- web/rainmaker/dev-packages/egov-boilerplate-dev/src/config/common.js
+++ web/rainmaker/dev-packages/egov-boilerplate-dev/src/config/common.js
@@ -1,5 +1,5 @@
 const commonConfig = {
-  MAP_API_KEY: "AIzaSyCH9PmCbk_mcpgijAAlTeltC4deOxC5wEM",
+  MAP_API_KEY: "AIzaSyAS6gWJ1oL6AhJnZnvjK6jmvQjeJBNvRx8",
   tenantId: process.env.REACT_APP_DEFAULT_TENANT_ID
   // forgotPasswordTenant: "pb.amritsar",
 };
