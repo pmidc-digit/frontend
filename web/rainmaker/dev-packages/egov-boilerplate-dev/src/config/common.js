7519cff091933f6ddda8e605f30e9027ce7a6b79
erramesh000 <erramesh000@gmail.com>
2023-12-15 10:25:58 +0530

    Fixed issues gmap mseva

--- web/rainmaker/dev-packages/egov-boilerplate-dev/src/config/common.js
+++ web/rainmaker/dev-packages/egov-boilerplate-dev/src/config/common.js
@@ -1,5 +1,5 @@
 const commonConfig = {
-  MAP_API_KEY: "AIzaSyAS6gWJ1oL6AhJnZnvjK6jmvQjeJBNvRx8",
+  MAP_API_KEY: "AIzaSyDjSk-Bw1OuiOE-5K7jsP6KGN2rbcN7iBk",
   tenantId: process.env.REACT_APP_DEFAULT_TENANT_ID
   // forgotPasswordTenant: "pb.amritsar",
 };
--- web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-config/app-config.js
+++ web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-config/app-config.js
@@ -1,2 +1,2 @@
 export const defaultLocation = { lat: 30.7333, lng: 76.7794 };
-export const MAP_API_KEY = "AIzaSyAS6gWJ1oL6AhJnZnvjK6jmvQjeJBNvRx8";
+export const MAP_API_KEY = "AIzaSyDjSk-Bw1OuiOE-5K7jsP6KGN2rbcN7iBk";
