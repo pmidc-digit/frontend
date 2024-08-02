3f4953f82413d1bdaa2f8c0bb117295f786fa563
erramesh000 <erramesh000@gmail.com>
2024-08-02 17:06:20 +0530

    updated property.creationReason update mobile dialog

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/UpdateMobile/updateMobileDialog.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/UpdateMobile/updateMobileDialog.js
@@ -141,12 +141,11 @@ export default class UpdateMobileDialog extends React.Component {
     var myHeaders = new Headers();
     let { property, propertyNumbers } = this.props;
     const { mobileNumber } = this.state.fields;
-
     if (property && property.owners && property.owners.length > 0) {
       property.owners.map(owner => {
         if (owner.uuid == propertyNumbers.uuid) {
           owner.mobileNumber = mobileNumber.value;
-         // property.creationReason = "UPDATE";
+          property.creationReason = "UPDATE";
           let documents = this.state.documents.filter(document => document.uploaded) || [];
           if (property.documents) {
             let docuNames = documents.map(doc => doc.code);
