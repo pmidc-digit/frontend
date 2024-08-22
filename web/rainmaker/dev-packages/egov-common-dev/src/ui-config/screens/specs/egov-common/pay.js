9925b8c580fac95aa432efb9a2594eef859dc80c
erramesh000 <erramesh000@gmail.com>
2024-08-22 14:22:43 +0530

    Fixed TenantId issues

--- web/rainmaker/dev-packages/egov-common-dev/src/ui-config/screens/specs/egov-common/pay.js
+++ web/rainmaker/dev-packages/egov-common-dev/src/ui-config/screens/specs/egov-common/pay.js
@@ -18,6 +18,7 @@ import estimateDetails from "./payResource/estimate-details";
 import { footer } from "./payResource/footer";
 import g8Details from "./payResource/g8-details";
 import arrearsCard from "./payResource/arrears-details";
+import { getTenantId } from "egov-ui-kit/utils/localStorageUtils";
 
 export const getHeader = (state) => {
     const uiCommonPayConfig = get(state.screenConfiguration.preparedFinalObject, "commonPayInfo");
@@ -45,10 +46,13 @@ export const getHeader = (state) => {
 
 
 const fetchBill = async (action, state, dispatch, consumerCode, tenantId, billBusinessService) => {
+   
+    debugger;
+    tenantId = getTenantId();
     await getBusinessServiceMdmsData(dispatch, tenantId);
 
     await generateBill(dispatch, consumerCode, tenantId, billBusinessService);
-
+     debugger;
     let payload = get(state, "screenConfiguration.preparedFinalObject.ReceiptTemp[0].Bill[0].billDetails[0]");
     let totalAmount = get(state, "screenConfiguration.preparedFinalObject.ReceiptTemp[0].Bill[0]");
 
@@ -132,10 +136,10 @@ const fetchBill = async (action, state, dispatch, consumerCode, tenantId, billBu
     dispatch(prepareFinalObject("ReceiptTemp[0].instrument.instrumentType.name", "Cash"));
 
     //set tenantId
-    dispatch(prepareFinalObject("ReceiptTemp[0].tenantId", tenantId));
+    dispatch(prepareFinalObject("ReceiptTemp[0].tenantId", getTenantId()));
 
     //set tenantId in instrument
-    dispatch(prepareFinalObject("ReceiptTemp[0].instrument.tenantId", tenantId));
+    dispatch(prepareFinalObject("ReceiptTemp[0].instrument.tenantId", getTenantId()));
 
     // Handling Negative amount
     if (get(totalAmount, "totalAmount") != undefined) {
@@ -153,9 +157,10 @@ const screenConfig = {
     uiFramework: "material-ui",
     name: "pay",
     beforeInitScreen: (action, state, dispatch) => {
+        debugger;
         dispatch(unMountScreen("acknowledgement"));
         let consumerCode = getQueryArg(window.location.href, "consumerCode");
-        let tenantId = getQueryArg(window.location.href, "tenantId");
+        let tenantId = getTenantId();
         let businessService = getQueryArg(window.location.href, "businessService");
         fetchBill(action, state, dispatch, consumerCode, tenantId, businessService);
         localStorage.setItem('pay-businessService', businessService);
@@ -175,7 +180,7 @@ const screenConfig = {
         //         set(action, "screenConfig.components.div.children.headerDiv.children.header" ,header) 
         //     }
         // );
-        // const data = getPaymentCard(state);    
+        // const data = state;    
         // set(action, "screenConfig.components.div.children.formwizardFirstStep", data);
         return action;
     },
