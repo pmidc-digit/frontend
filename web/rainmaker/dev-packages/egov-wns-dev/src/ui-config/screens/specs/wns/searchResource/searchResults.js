4ddf1c211954f9020ddd16a6cfed402335aeb194
anujkit <46367271+anujkit@users.noreply.github.com>
2024-11-27 17:38:47 +0530

    Update searchResults.js
    For Collect Button

--- web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/searchResource/searchResults.js
+++ web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/searchResource/searchResults.js
@@ -81,7 +81,7 @@ export const searchResults = {
         options: {
           filter: false,
           customBodyRender: (value, data) => {
-            if (data.rowData[4] !== undefined && typeof parseFloat(data.rowData[4]) === 'number' && parseFloat(data.rowData[4]) > 0) {
+            if (data.rowData[5] !== undefined && typeof parseFloat(data.rowData[5]) === 'number' && parseFloat(data.rowData[5]) > 0) {
               return (
                 <div className="linkStyle" onClick={() => getViewBillDetails(data)} style={{ color: '#fe7a51', textTransform: 'uppercase' }}>
                   <LabelContainer
