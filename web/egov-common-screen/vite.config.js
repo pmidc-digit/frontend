9fb42c03c6c620056bb4bd21db15db015aa4c957
erramesh000 <erramesh000@gmail.com>
2025-01-24 09:43:14 +0530

    Update vite config file

--- web/egov-common-screen/vite.config.js
+++ web/egov-common-screen/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: 'localhost', // Or '0.0.0.0' for access from other devices
+    port: 3000,        // Default Vite port
+  },
 })
