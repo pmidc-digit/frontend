f74f77e529d421d0940212a4aae19bdd8284181b
erramesh000 <erramesh000@gmail.com>
2025-01-28 14:45:03 +0530

    update docker file

--- web/egov-common-screen/vite.config.js
+++ web/egov-common-screen/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/common-screen',
   server: {
     host: 'localhost',
     port: 3000,
