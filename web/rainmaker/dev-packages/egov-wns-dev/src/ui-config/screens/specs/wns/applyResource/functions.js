b42cfb4b25b894d3c4af0bba4dc3d05b4ac20dfa
erramesh000 <erramesh000@gmail.com>
2025-04-30 16:09:30 +0530

    Updated Ludhiana WS and SW

--- web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/applyResource/functions.js
+++ web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/applyResource/functions.js
@@ -1,14 +1,22 @@
 import get from "lodash/get";
 import { handleScreenConfigurationFieldChange as handleField, prepareFinalObject } from "egov-ui-framework/ui-redux/screen-configuration/actions";
 import { toggleSnackbar } from "egov-ui-framework/ui-redux/screen-configuration/actions";
-import { getPropertyResults, isActiveProperty, showHideFieldsFirstStep, getMCLpropertyResult } from "../../../../../ui-utils/commons";
+import { getPropertyResults, isActiveProperty, showHideFieldsFirstStep } from "../../../../../ui-utils/commons";
 import { getUserInfo, getTenantIdCommon } from "egov-ui-kit/utils/localStorageUtils";
+import { httpRequest } from "../../../../../ui-utils/api";
+import store from "ui-redux/store";
+import set from 'lodash/set';
+
+import { getQueryArg, validateFields } from "egov-ui-framework/ui-utils/commons";
+import { getQueryRedirectUrl } from "../searchResource/searchResults";
+import { setRoute } from "egov-ui-framework/ui-redux/app/actions";
 
 export const propertySearchApiCall = async (state, dispatch) => {
   showHideFields(dispatch, false);
   let tenantId = getTenantIdCommon();
   let queryObject = [{ key: "tenantId", value: tenantId }];
   let searchScreenObject = get(state.screenConfiguration.preparedFinalObject, "searchScreen", {});
+
   dispatch(
     handleField(
       "apply",
@@ -36,6 +44,103 @@ export const propertySearchApiCall = async (state, dispatch) => {
     Object.values(searchScreenObject).every(x => x === "")
   ) {
     dispatch(toggleSnackbar(true, { labelKey: "WS_FILL_REQUIRED_FIELDS", labelName: "Please fill required details" }, "warning"));
+    if (tenantId != "pb.testing") {
+      alert("testing");
+    }
+    else {
+      let propertyPayload = {
+        "address": {
+          "city": "testing",
+          "locality": {
+            "code": "SC7"
+          },
+          "doorNo": "2",
+          "buildingName": "asas"
+        },
+        "propertyType": "BUILTUP.INDEPENDENTPROPERTY",
+        "subUsageCategory": "",
+        "landArea": 111,
+        "owners": [
+          {
+            "mobileNumber": "9888990012",
+            "name": "test",
+            "correspondenceAddress": "2, asas, Adarsh Nagar - B3, testing",
+            "fatherOrHusbandName": "testing",
+            "relationship": "FATHER",
+            "gender": "MALE",
+            "ownerType": "NONE",
+            "sameAsPeropertyAddress": true,
+            "status": "ACTIVE"
+          }
+        ],
+        "superBuiltUpArea": "111",
+        "usageCategory": "RESIDENTIAL",
+        "surveyId": "1223",
+        "ownershipCategory": "INDIVIDUAL.SINGLEOWNER",
+        "channel": "SYSTEM",
+        "source": "WATER_CHARGES",
+        "noOfFloors": 1,
+        "tenantId": "pb.testing",
+        "additionalDetails": {
+          "isRainwaterHarvesting": false,
+          "subUsageCategory": ""
+        },
+        "creationReason": "CREATE"
+      }
+      debugger;
+      let payload = null;
+
+      payload = await httpRequest(
+        "post",
+        "/property-services/property/_create",
+        "_update",
+        [],
+        { Property: propertyPayload }
+
+      );
+
+      console.log("payload", payload);
+      if (payload.Properties && payload.Properties.length > 0) {
+        propertyPayload = payload.Properties[0];
+        let isFromWorkflowDetails = {
+          action: "SUBMIT",
+          assignes: null,
+          businessId: propertyPayload.propertyId,
+          businessService: "PT.CREATEWITHWNS",
+          comment: null,
+          documents: null,
+          moduleName: "PT",
+          state: null,
+          tenantId: propertyPayload.tenantId
+        };
+        set(propertyPayload, "workflow", isFromWorkflowDetails);
+        payload.Properties[0].creationReason = 'UPDATE';
+        payload = await httpRequest(
+          "post",
+          "/property-services/property/_update",
+          "_update",
+          [],
+          { Property: propertyPayload }
+        );
+        if (payload) {
+          setTimeout(() => {
+            // const isMode = getQueryArg(window.location.href, "mode");
+            // if (isMode === "MODIFY") {
+            //   store.dispatch(
+            //     setRoute(`${getQueryRedirectUrl()}&propertyId=${payload.Properties[0].propertyId}`)
+            //   )
+            // } else {
+            store.dispatch(
+              setRoute(`${getQueryRedirectUrl()}?propertyId=${payload.Properties[0].propertyId}&tenantId=${propertyPayload.tenantId}`)
+            )
+
+            location.reload();
+            // }
+          }, 3000);
+        }
+      }
+    }
+
   } else {
     for (var key in searchScreenObject) {
       if (searchScreenObject.hasOwnProperty(key) && searchScreenObject[key].trim() !== "") {
@@ -60,36 +165,36 @@ export const propertySearchApiCall = async (state, dispatch) => {
       let response = await getPropertyResults(queryObject, dispatch);
       if (response && response.Properties.length > 0) {
         let propertyData = response.Properties[0];
-        if(!isActiveProperty(propertyData)){
-          dispatch(toggleSnackbar(true, { labelKey: `ERR_WS_PROP_STATUS_${propertyData.status}`, labelName: `Property Status is ${propertyData.status}` }, "warning"));     
-          showHideFieldsFirstStep(dispatch,propertyData.propertyId,false); 
-          dispatch(prepareFinalObject("applyScreen.property", propertyData))         
-        }else{          
-         let contractedCorAddress = "";
+        if (!isActiveProperty(propertyData)) {
+          dispatch(toggleSnackbar(true, { labelKey: `ERR_WS_PROP_STATUS_${propertyData.status}`, labelName: `Property Status is ${propertyData.status}` }, "warning"));
+          showHideFieldsFirstStep(dispatch, propertyData.propertyId, false);
+          dispatch(prepareFinalObject("applyScreen.property", propertyData))
+        } else {
+          let contractedCorAddress = "";
 
-          if(propertyData.address.doorNo !== null && propertyData.address.doorNo !== ""){
+          if (propertyData.address.doorNo !== null && propertyData.address.doorNo !== "") {
             contractedCorAddress += propertyData.address.doorNo + ", ";
           }
-          if(propertyData.address.buildingName !== null && propertyData.address.buildingName !== ""){
+          if (propertyData.address.buildingName !== null && propertyData.address.buildingName !== "") {
             contractedCorAddress += propertyData.address.buildingName + ", ";
-          }        
+          }
           contractedCorAddress += propertyData.address.locality.name + ", " + propertyData.address.city;
 
-          for(var i=0; i<propertyData.owners.length;i++){ 
-            if(propertyData.owners[i].correspondenceAddress == 'NA' || propertyData.owners[i].correspondenceAddress == null || propertyData.owners[i].correspondenceAddress == ""){
-              if(propertyData.owners[i].permanentAddress == 'NA' || propertyData.owners[i].permanentAddress == null || propertyData.owners[i].permanentAddress == ""){
-                propertyData.owners[i].correspondenceAddress = contractedCorAddress;
-              }else{
-                propertyData.owners[i].correspondenceAddress = propertyData.owners[i].permanentAddress;
-              }
-            } 
-              if(propertyData && propertyData.owners && propertyData.owners.length > 0) {
-                propertyData.owners = propertyData.owners.filter(owner => owner.status == "ACTIVE");
-              }   
-          }
-        if(propertyData.units == "NA" && propertyData.additionalDetails && propertyData.additionalDetails.subUsageCategory) {
+          for (var i = 0; i < propertyData.owners.length; i++) {
+            if (propertyData.owners[i].correspondenceAddress == 'NA' || propertyData.owners[i].correspondenceAddress == null || propertyData.owners[i].correspondenceAddress == "") {
+              if (propertyData.owners[i].permanentAddress == 'NA' || propertyData.owners[i].permanentAddress == null || propertyData.owners[i].permanentAddress == "") {
+                propertyData.owners[i].correspondenceAddress = contractedCorAddress;
+              } else {
+                propertyData.owners[i].correspondenceAddress = propertyData.owners[i].permanentAddress;
+              }
+            }
+            if (propertyData && propertyData.owners && propertyData.owners.length > 0) {
+              propertyData.owners = propertyData.owners.filter(owner => owner.status == "ACTIVE");
+            }
+          }
+          if (propertyData.units == "NA" && propertyData.additionalDetails && propertyData.additionalDetails.subUsageCategory) {
             propertyData.units = [];
-            propertyData.units.push({usageCategory: propertyData.additionalDetails.subUsageCategory})
+            propertyData.units.push({ usageCategory: propertyData.additionalDetails.subUsageCategory })
           }
           dispatch(prepareFinalObject("applyScreen.property", propertyData))
           showHideFields(dispatch, true);
@@ -103,19 +208,6 @@ export const propertySearchApiCall = async (state, dispatch) => {
       console.log(err)
     }
   }
-}
-
-export const propertyMCLSearchApiCall = async (state, dispatch) => {
-  
-  let tenantId = getTenantIdCommon();
-  let queryObject = [{ key: "tenantId", value: tenantId }];
-  let surveyUUID = get(state.screenConfiguration.preparedFinalObject, "searchScreen.propertyIds", {});
-
-
-  let response = await getMCLpropertyResult(surveyUUID, dispatch);
-  debugger
-  console.log("response", response)
-
 }
 
 const showHideFields = (dispatch, value) => {
--- web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/searchResource/searchResults.js
+++ web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/searchResource/searchResults.js
@@ -2,9 +2,39 @@ import React from "react";
 import { sortByEpoch, getEpochForDate } from "../../utils";
 import './index.css'
 import LabelContainer from "egov-ui-framework/ui-containers/LabelContainer";
+import { getQueryArg, getStatusKey } from "egov-ui-framework/ui-utils/commons";
 import { setRoute } from "egov-ui-framework/ui-redux/app/actions";
 import store from "ui-redux/store";
+import get from "lodash/get";
 
+export const getQueryRedirectUrl = () => {
+  let url = getQueryArg(window.location.href, "redirectUrl");
+  url = '/wns/apply?propertyId=PT-1012-2180005&tenantId=pb.testing'
+  const isMode = getQueryArg(window.location.href, "mode");
+  if (isMode === "MODIFY") {
+    const connectionNumber = getQueryArg(window.location.href, "connectionNumber");
+    const tenantId = getQueryArg(window.location.href, "tenantId");
+    const action = getQueryArg(window.location.href, "action");
+    const modeaction = getQueryArg(window.location.href, "modeaction");
+    let returnUrl = `${url}&connectionNumber=${connectionNumber}&tenantId=${tenantId}&action=${action}&mode=${isMode}`;
+    returnUrl = modeaction ? returnUrl + '&modeaction=' + modeaction : returnUrl;
+    return returnUrl;
+  } else {
+    //url = url.includes('?') ? url : url + '?';
+
+    url = 'apply'
+    let applicationNo = getQueryArg(window.location.href, "applicationNumber");
+    const connectionNo = getQueryArg(window.location.href, "connectionNumber");
+    const actionType = getQueryArg(window.location.href, "action");
+    url = applicationNo && !url.includes('applicationNumber') ? url + `&applicationNumber=${applicationNo}` : url;
+    url = connectionNo && !url.includes('connectionNumber') ? url + `&connectionNumber=${connectionNo}` : url;
+    url = actionType && !url.includes('action') ? url + `&action=${actionType}` : url;
+    return url;
+  }
+
+
+
+};
 
 export const searchResults = {
   uiFramework: "custom-molecules",
@@ -15,7 +45,7 @@ export const searchResults = {
     columns: [
       {
         name: "Service",
-        labelKey: "WS_COMMON_TABLE_COL_SERVICE_LABEL", 
+        labelKey: "WS_COMMON_TABLE_COL_SERVICE_LABEL",
         options: {
           filter: false,
           customBodyRender: value => (
@@ -27,7 +57,7 @@ export const searchResults = {
       },
       {
         name: "Consumer No",
-        labelKey: "WS_COMMON_TABLE_COL_CONSUMER_NO_LABEL", 
+        labelKey: "WS_COMMON_TABLE_COL_CONSUMER_NO_LABEL",
         options: {
           filter: false,
           customBodyRender: (value, index) => (
@@ -37,12 +67,12 @@ export const searchResults = {
           )
         }
       },
-      {name : "Owner Name",labelKey: "WS_COMMON_TABLE_COL_OWN_NAME_LABEL" },
-      {name : "Mobile Number",labelKey: "WS_HOME_SEARCH_RESULTS_OWN_MOB_LABEL" },
-      {name : "Status",labelKey: "WS_COMMON_TABLE_COL_STATUS_LABEL" },
-      {name : "Due",labelKey: "WS_COMMON_TABLE_COL_DUE_LABEL" },
-      {name : "Address",labelKey: "WS_COMMON_TABLE_COL_ADDRESS" },
-      {name : "Due Date",labelKey: "WS_COMMON_TABLE_COL_DUE_DATE_LABEL" },
+      { name: "Owner Name", labelKey: "WS_COMMON_TABLE_COL_OWN_NAME_LABEL" },
+      { name: "Mobile Number", labelKey: "WS_HOME_SEARCH_RESULTS_OWN_MOB_LABEL" },
+      { name: "Status", labelKey: "WS_COMMON_TABLE_COL_STATUS_LABEL" },
+      { name: "Due", labelKey: "WS_COMMON_TABLE_COL_DUE_LABEL" },
+      { name: "Address", labelKey: "WS_COMMON_TABLE_COL_ADDRESS" },
+      { name: "Due Date", labelKey: "WS_COMMON_TABLE_COL_DUE_DATE_LABEL" },
       {
         name: "Action",
         labelKey: "WS_COMMON_TABLE_COL_ACTION_LABEL",
@@ -50,9 +80,9 @@ export const searchResults = {
           filter: false,
           customBodyRender: (value, data) => {
             debugger;
-            
+
             if (data.rowData[5] !== undefined && typeof data.rowData[5] === 'number' && data.rowData[5] >= 0) {
-                
+
               return (
                 <div className="linkStyle" onClick={() => getViewBillDetails(data)} style={{ color: '#fe7a51', textTransform: 'uppercase' }}>
                   <LabelContainer
@@ -92,9 +122,9 @@ export const searchResults = {
           display: false
         }
       }
-      
+
     ],
-    title: {labelKey:"WS_HOME_SEARCH_RESULTS_TABLE_HEADING", labelName:"Search Results for Water & Sewerage Connections"},
+    title: { labelKey: "WS_HOME_SEARCH_RESULTS_TABLE_HEADING", labelName: "Search Results for Water & Sewerage Connections" },
     options: {
       filter: false,
       download: false,
@@ -124,10 +154,10 @@ export const searchResults = {
 const getConnectionDetails = data => {
   debugger;
   let legacy
-  if(data.rowData[11]=== true){
-      legacy = true
-  }else{
-    legacy =false
+  if (data.rowData[11] === true) {
+    legacy = true
+  } else {
+    legacy = false
   }
   store.dispatch(
     setRoute(`connection-details?connectionNumber=${data.rowData[1]}&tenantId=${data.rowData[9]}&service=${data.rowData[0]}&connectionType=${data.rowData[10]}&due=${data.rowData[5]}&legacy=${legacy}`)
@@ -136,6 +166,6 @@ const getConnectionDetails = data => {
 
 const getViewBillDetails = data => {
   store.dispatch(
-    setRoute( `viewBill?connectionNumber=${data.rowData[1]}&tenantId=${data.rowData[9]}&service=${data.rowData[0]}&connectionType=${data.rowData[10]}`)
+    setRoute(`viewBill?connectionNumber=${data.rowData[1]}&tenantId=${data.rowData[9]}&service=${data.rowData[0]}&connectionType=${data.rowData[10]}`)
   )
 }
