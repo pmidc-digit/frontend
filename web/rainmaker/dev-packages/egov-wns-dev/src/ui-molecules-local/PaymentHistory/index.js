eee83b7a1c51c9c5e065bfa57b54ac17467a5ec3
erramesh000 <erramesh000@gmail.com>
2025-06-04 10:46:27 +0530

    Fixed issues  Payment Receipt WS and SW

--- web/rainmaker/dev-packages/egov-wns-dev/src/ui-molecules-local/PaymentHistory/index.js
+++ web/rainmaker/dev-packages/egov-wns-dev/src/ui-molecules-local/PaymentHistory/index.js
@@ -324,6 +324,7 @@ class PaymentHistory extends Component {
         else {
           payloadReceiptDetails.Payments[0].ownerNumber = payloadReceiptDetails.Payments[0].ownerNumber.join(", ");
           payloadReceiptDetails.Payments[0].ownername = payloadReceiptDetails.Payments[0].ownername.join(", ");
+          payloadReceiptDetails.Payments[0].address = payloadReceiptDetails.Payments[0].address.replace(/[^a-zA-Z0-9 ]/g, ' ');
           httpRequest("post", DOWNLOADRECEIPT.GET.URL, DOWNLOADRECEIPT.GET.ACTION, queryStr, { Payments: payloadReceiptDetails.Payments }, { 'Accept': 'application/json' }, { responseType: 'arraybuffer' })
             .then(res => {
               res.filestoreIds[0]
