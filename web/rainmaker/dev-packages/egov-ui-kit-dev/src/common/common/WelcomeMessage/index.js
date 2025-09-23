15f2ce7fb4507298d61ed977d21a0fdbdf474a43
erramesh000 <erramesh000@gmail.com>
2025-09-23 16:03:10 +0530

    Updated css Mission Chardi Kala Pop Up

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/common/WelcomeMessage/index.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/common/WelcomeMessage/index.js
@@ -7,36 +7,50 @@ const styles = {
   logoutContentStyle: { textAlign: "center", padding: "24px 20px" },
 };
 
+function hidePopup(event) {
+  if (event) event.preventDefault();
+  document.querySelector(".popup-overlay").style.display = "none";
+  document.querySelector(".mypop").style.display = "none";
+}
+
 const WelcomeMessage = ({ WCPopupClose, WCPopupOpen, title, body }) => {
   const actions = [
     <Button
       label={"test"}
       backgroundColor={"#fff"}
-      style={{ boxShadow: "none", display:"none" }}
-    />
+      style={{ boxShadow: "none", display: "none" }}
+    />,
   ];
+
   return (
-    <Dialog
-      open={WCPopupOpen}
-      title={
-        <Label
-          label={title}
-          bold={true}
-          color="rgba(0, 0, 0, 0.8700000047683716)"
-          fontSize="20px"
-          labelStyle={{ padding: "16px 0px 0px 24px" }}
-        />
-      }
-      children={[
-        <Image className="whatsApp-Image" source={`${body}`} />
-      ]}
-      handleClose={WCPopupClose}
-      actions={actions}
-      contentClassName={"wc-popup"}
-      contentStyle={{ width: "90%" }}
-      isClose={true}
-      isImage ={true}
-    />
+    <React.Fragment>
+      <Dialog
+        open={WCPopupOpen}
+        title={
+          <Label
+            label={title}
+            bold={true}
+            color="rgba(0, 0, 0, 0.8700000047683716)"
+            fontSize="20px"
+            labelStyle={{ padding: "16px 0px 0px 24px" }}
+          />
+        }
+        children={[<Image className="whatsApp-Image" source={`${body}`} />]}
+        handleClose={WCPopupClose}
+        actions={actions}
+        contentClassName={"wc-popup"}
+        contentStyle={{ width: "90%" }}
+        isClose={true}
+        isImage={true}
+      />
+      <div className="popup-overlay"></div>
+      <div className="mypop">
+        <a href="#" className="popup-close" onClick={hidePopup}>
+          ✕
+        </a>
+        <img src="https://lgpunjab.gov.in/mf.png" alt="Punjab Plantation" />
+      </div>
+    </React.Fragment>
   );
 };
 
