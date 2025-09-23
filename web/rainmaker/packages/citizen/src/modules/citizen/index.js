f7869fb304363061adb17a4c98ea20ea92f8b450
erramesh000 <erramesh000@gmail.com>
2025-09-23 15:52:35 +0530

    Revert "Updated css  Mission Chardi Kala Pop Up"

--- web/rainmaker/packages/citizen/src/modules/citizen/index.js
+++ web/rainmaker/packages/citizen/src/modules/citizen/index.js
@@ -1,23 +1,8 @@
 import React from "react";
 import { RenderRoutes } from "modules/common";
 
-function hidePopup(event) {
-  if (event) event.preventDefault();
-  document.querySelector('.popup-overlay').style.display = 'none';
-  document.querySelector('.mypop').style.display = 'none';
-}
-
 const Citizen = ({ match, routes = [] }) => {
-  return (
-    <React.Fragment>
-      <RenderRoutes match={match} routes={routes} />
-      <div className="popup-overlay"></div>
-      <div className="mypop">
-        <a href="#" className="popup-close" onClick={hidePopup}>✕</a>
-        <img src="https://lgpunjab.gov.in/mf.png" alt="Punjab Plantation" />
-      </div>
-    </React.Fragment>
-  );
+  return <RenderRoutes match={match} routes={routes} />;
 };
 
 export default Citizen;
