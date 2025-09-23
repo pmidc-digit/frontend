24a8d8d560c98bd01420beebb7739d3f902c1ba1
erramesh000 <erramesh000@gmail.com>
2025-09-23 15:07:21 +0530

    Updated Mission Chardi Kala Pop Up

--- web/rainmaker/packages/citizen/src/modules/citizen/index.js
+++ web/rainmaker/packages/citizen/src/modules/citizen/index.js
@@ -1,8 +1,23 @@
 import React from "react";
 import { RenderRoutes } from "modules/common";
 
+function hidePopup(event) {
+  if (event) event.preventDefault();
+  document.querySelector('.popup-overlay').style.display = 'none';
+  document.querySelector('.mypop').style.display = 'none';
+}
+
 const Citizen = ({ match, routes = [] }) => {
-  return <RenderRoutes match={match} routes={routes} />;
+  return (
+    <React.Fragment>
+      <RenderRoutes match={match} routes={routes} />
+      <div className="popup-overlay"></div>
+      <div className="mypop">
+        <a href="#" className="popup-close" onClick={hidePopup}>✕</a>
+        <img src="https://lgpunjab.gov.in/mf.png" alt="Punjab Plantation" />
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Citizen;
