612ab4298416945fa018b1842b18cc3030448c13
erramesh000 <erramesh000@gmail.com>
2025-11-06 11:39:38 +0530

    update pop up check  payer number

--- web/rainmaker/dev-packages/egov-common-dev/src/ui-containers-local/EstimateCardContainer/index.js
+++ web/rainmaker/dev-packages/egov-common-dev/src/ui-containers-local/EstimateCardContainer/index.js
@@ -1,12 +1,78 @@
 import React, { Component } from "react";
 import { FeesEstimateCard } from "../../ui-molecules-local";
+import { Dialog } from "components";
 import { connect } from "react-redux";
 import get from "lodash/get";
 import orderBy from "lodash/orderBy";
 
 class EstimateCardContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { welcomePopupOpen: false };
+    this._welcomeTimer = null;
+  }
+
+  componentDidMount() {
+    // Show a welcome popup 2 seconds after landing on Payment Collection Details
+    if (this._welcomeTimer) {
+      clearTimeout(this._welcomeTimer);
+    }
+    this._welcomeTimer = setTimeout(() => {
+      this.setState({ welcomePopupOpen: true });
+      this._welcomeTimer = null;
+    }, 2000);
+  }
+
+  componentWillUnmount() {
+    if (this._welcomeTimer) {
+      clearTimeout(this._welcomeTimer);
+      this._welcomeTimer = null;
+    }
+  }
+
+  closeWelcomePopup = () => {
+    if (this._welcomeTimer) {
+      clearTimeout(this._welcomeTimer);
+      this._welcomeTimer = null;
+    }
+    this.setState({ welcomePopupOpen: false });
+  }
+
   render() {
-    return <FeesEstimateCard estimate={this.props.estimate} isArrears={this.props.isArrears} />;
+    const { estimate, isArrears } = this.props;
+    const { welcomePopupOpen } = this.state;
+    return (
+      <React.Fragment>
+        <FeesEstimateCard estimate={estimate} isArrears={isArrears} />
+
+        <Dialog
+          className="pt-warning-popup"
+          open={welcomePopupOpen}
+          isClose={true}
+          // title={"NOC_FEE_ESTIMATE_HEADER_payer_mobile"}
+          handleClose={this.closeWelcomePopup}
+          titleStyle={{ padding: "2%", backgroundColor: "white" }}
+          actionsContainerStyle={{ padding: "2%", backgroundColor: "white" }}
+          bodyStyle={{ padding: "0% 2% 2% 2%", backgroundColor: "white" }}
+        >
+          <div style={{ padding: "10px", textAlign: "center" }}>
+            <div style={{ color: "rgba(0, 0, 0, 0.87)", fontSize: "14px", marginBottom: "4px" }}>
+              <label title="{PAYMER_MOBILE_NO_MSG}"></label>
+            </div>
+          </div>
+          <div className="pt-warning-button-container" style={{ textAlign: "center" }}>
+            <button
+              type="button"
+              style={{ width: "100%", marginTop: "10px", color: "white", fontWeight: 500 }}
+              className={"button-verify-link"}
+              onClick={this.closeWelcomePopup}
+            >
+              OK
+            </button>
+          </div>
+        </Dialog>
+      </React.Fragment>
+    );
   }
 }
 
@@ -19,7 +85,7 @@ const formatTaxHeaders = (billDetail = {}) => {
 
   let formattedFees = []
   const { billAccountDetails = [] } = billDetail;
-const billAccountDetailsSorted=  orderBy(
+  const billAccountDetailsSorted = orderBy(
     billAccountDetails,
     ["amount"],
     ["asce"]);
@@ -51,15 +117,15 @@ const mapStateToProps = (state, ownProps) => {
   const billDetails = get(screenConfiguration, "preparedFinalObject.ReceiptTemp[0].Bill[0].billDetails", []);
   const isArrears = get(screenConfiguration, "preparedFinalObject.isArrears");
   let totalAmount = 0;
-  let arrears=0;
+  let arrears = 0;
   for (let billDetail of billDetails) {
     totalAmount += billDetail.amount;
 
   }
-if(totalAmount>0){
-  arrears=totalAmount-billDetails[0].amount;
-  arrears = arrears.toFixed(2);
-}
+  if (totalAmount > 0) {
+    arrears = totalAmount - billDetails[0].amount;
+    arrears = arrears.toFixed(2);
+  }
   const estimate = {
     header: { labelName: "Fee Estimate", labelKey: "NOC_FEE_ESTIMATE_HEADER" },
     fees,
