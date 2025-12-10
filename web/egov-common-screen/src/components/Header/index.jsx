dc4d81f5fa2e11cf5a02ced8827c3b8eb7b15cf5
erramesh000 <erramesh000@gmail.com>
2025-12-10 15:18:22 +0530

    Update Citizen login link under common screen

--- web/egov-common-screen/src/components/Header/index.jsx
+++ web/egov-common-screen/src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../myindex.css';
 import rightImage from "../../assets/img/pmidc - final logo.png";
 import leftImage from "../../assets/img/Govt. of Punjab.png";
-import slideone from '../../assets/img/pmidcgurughar.jpg';
+//import slideone from '../../assets/img/pmidcgurughar.jpg';
 import homeicon from '../../assets/img/homepage-icon.png';
 
 
@@ -41,20 +41,11 @@ export default function Header() {
                         </div>
                         <div className="col-sm-12 col-md-6">
                             <div className="rightharder">
-                                <a href="https://mseva.lgpunjab.gov.in/employee/language-selection" target="_blank">Employee Login </a><img id="rightimg" src={rightImage} />
+                                <a href="https://mseva.lgpunjab.gov.in/employee/language-selection" target="_blank">Employee Login </a> | <a href="https://mseva.lgpunjab.gov.in/citizen/language-selection" target="_blank">Citizen Login </a><img id="rightimg" src={rightImage} />
                             </div>
                         </div>
                     </div>
-                    {/* <div className="row">
-              <div className="col-sm-12 col-md-12">
-                <marquee id="note">Dear Citizen , One Time settlement for Property Tax has been implemented in mSeva. You requested check and Re-Assess your property before paying tax .</marquee>
-              </div>
-            </div> */}
-                    {/* <div className="row">
-                        <div className="col-sm-12 col-md-12">
-                            <img className="d-block w-100" src={slideone} alt="PMIDC" />
-                        </div>
-                    </div> */}
+
 
                 </div>
             </div>
