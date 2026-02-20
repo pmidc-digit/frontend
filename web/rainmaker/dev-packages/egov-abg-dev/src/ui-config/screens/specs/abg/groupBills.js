ed830e348b1677591c8f59bff4e75f897dc73c1c
anujkit <chauhan.chauhan5@gmail.com>
2026-02-20 17:41:07 +0530

    Integrated Billing

--- web/rainmaker/dev-packages/egov-abg-dev/src/ui-config/screens/specs/abg/groupBills.js
+++ web/rainmaker/dev-packages/egov-abg-dev/src/ui-config/screens/specs/abg/groupBills.js
@@ -145,7 +145,8 @@ const abgSearchAndResult = {
                     ? `/egov-ui-framework/abg/groupBillDownloads`
                     : `/abg/groupBillDownloads`
               },
-              visible: (process.env.REACT_APP_NAME === "Citizen") ? false : true
+              //visible: (process.env.REACT_APP_NAME === "Citizen") ? false : true
+              visible: false 
             }
           }
         },
