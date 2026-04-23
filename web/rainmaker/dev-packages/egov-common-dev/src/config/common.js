d34b24f44b7b04e4d1117e6f078acaba551ef3f5
anujkit <chauhan.chauhan5@gmail.com>
2026-04-23 16:20:16 +0530

    PI-21257 update google API Key

--- web/rainmaker/dev-packages/egov-common-dev/src/config/common.js
+++ web/rainmaker/dev-packages/egov-common-dev/src/config/common.js
@@ -1,5 +1,5 @@
 const commonConfig = {
-  MAP_API_KEY: "AIzaSyCH9PmCbk_mcpgijAAlTeltC4deOxC5wEM",
+  MAP_API_KEY: "AIzaSyB5xqCmrgz-IL-mPQiy7PI_mA4drmyr2fo",
   tenantId: process.env.REACT_APP_DEFAULT_TENANT_ID
   // forgotPasswordTenant: "pb.amritsar",
 };
