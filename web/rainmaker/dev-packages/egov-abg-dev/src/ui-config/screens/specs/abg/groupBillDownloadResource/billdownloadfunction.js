f94202988fdc12bfb160bb2c81c20d7247b277b2
anujkit <chauhan.chauhan5@gmail.com>
2026-07-20 17:07:39 +0530

    PI-21407

--- web/rainmaker/dev-packages/egov-abg-dev/src/ui-config/screens/specs/abg/groupBillDownloadResource/billdownloadfunction.js
+++ web/rainmaker/dev-packages/egov-abg-dev/src/ui-config/screens/specs/abg/groupBillDownloadResource/billdownloadfunction.js
@@ -104,6 +104,8 @@ export var searchApiCall = function (state, dispatch, limit, offset) {
           if (!localityNames) {
             localityNames = "-";
           }
+        } else if (item && item.group) {
+          localityNames = item.group;
         }
 
         return {
