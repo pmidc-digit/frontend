b8e7bd27e669a02b761eec2c380c93885f60a66d
anujkit <chauhan.chauhan5@gmail.com>
2026-08-05 23:25:45 +0530

    group bill download issue

--- web/rainmaker/dev-packages/egov-abg-dev/src/ui-config/screens/specs/abg/groupBillDownloadResource/billdownloadfunction.js
+++ web/rainmaker/dev-packages/egov-abg-dev/src/ui-config/screens/specs/abg/groupBillDownloadResource/billdownloadfunction.js
@@ -63,8 +63,11 @@ export var searchApiCall = function (state, dispatch, limit, offset) {
         return;
       }
 
-      var filteredBillData = billRecords.filter(function (item) {
-        return item.status === "DONE";
+      // var filteredBillData = billRecords.filter(function (item) {
+      //   return item.status === "DONE";
+      // });
+      var filteredBillData = billRecords.sort(function (a, b) {
+        return (b.lastmodifiedtime || 0) - (a.lastmodifiedtime || 0);
       });
       //console.table("tableData",filteredBillData);
       //make table data
@@ -141,6 +144,8 @@ export var searchApiCall = function (state, dispatch, limit, offset) {
             item && item.bussinessService
               ? item.bussinessService
               : "-",
+          ABG_STATUS : item && item.status 
+            ? item.status : "-"
         };
       });
 
@@ -204,10 +209,12 @@ const generateLocalityKey = (state, localityCode) => {
   const localitiesData = get(
     state,
     "screenConfiguration.preparedFinalObject.localitiesData",
-    {}
+    []
   );
-  let localityName = localitiesData.find((item => item.code === localityCode));
-  return localityName.name;
+  const localityName = Array.isArray(localitiesData)
+    ? localitiesData.find((item => item.code === localityCode))
+    : null;
+  return localityName ? localityName.name : "-";
 }
 const showHideTable = (booleanHideOrShow, dispatch) => {
   dispatch(
