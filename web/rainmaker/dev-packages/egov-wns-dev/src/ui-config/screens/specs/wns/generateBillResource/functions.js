eff694f3a3eb727b8bbed79ef64760c13db6160f
Anoushka Srivastava <92670920+anou1234@users.noreply.github.com>
2026-08-14 15:45:13 +0530

    addition of bill generation date for search bill response ws

--- web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/generateBillResource/functions.js
+++ web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/generateBillResource/functions.js
@@ -133,6 +133,7 @@ export const generateBillApiCall = async (state, dispatch) => {
         let locality, billingcycleStartdate, billingcycleEnddate, status, tenantId, billGenerationDate;
 
         response.billScheduler.map((element, index) => {
+          console.log(element ,"element")
           transactionType = element.transactionType;
           locality = element.locality ? element.locality : element.grup;
           billingcycleStartdate = convertEpochToDate(element.billingcycleStartdate);
@@ -335,7 +336,7 @@ export const searchBillApiCall = async (state, dispatch) => {
 
         let searchBillArray = [];
         let billRow = null;
-        let locality, billingcycleStartdate, billingcycleEnddate, status, tenantId, billformDate;
+        let locality, billingcycleStartdate, billingcycleEnddate, status, tenantId, billformDate, billGenerationDate;
         let recourdcount = response.billScheduler.length;
         response.billScheduler.map((element, index) => {
           transactionType = element.transactionType;
@@ -345,6 +346,7 @@ export const searchBillApiCall = async (state, dispatch) => {
           billingcycleEnddate = convertEpochToDate(element.billingcycleEnddate);
           status = element.status;
           tenantId = element.tenantId;
+          billGenerationDate = element.auditDetails && element.auditDetails.createdTime ? convertEpochToDate(element.auditDetails.createdTime) : "NA";
           billRow = {
             "transactionType": transactionType,
             "locality": locality,
@@ -354,7 +356,8 @@ export const searchBillApiCall = async (state, dispatch) => {
             "tenantId": tenantId,
             "recordcount": recourdcount,
             "service": servicety,
-            "billformDate": billformDate
+            "billformDate" : billformDate,
+            "billGenerationDate": billGenerationDate
           };
           searchBillArray.push(billRow);
         });
